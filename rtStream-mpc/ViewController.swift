2138716fea4c3f28efa732480b1871d3e24d9261
Jens Woltering <Jens.woltering@gmail.com>
2016-01-16 14:35:39 +0100

    Initial Commit

---
+++ rtStream-mpc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rtStream-mpc
+//
+//  Created by Jens Woltering on 16.01.16.
+//  Copyright © 2016 Jens Woltering. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ rtStream-mpcTests/rtStream_mpcTests.swift
@@ -0,0 +1,36 @@
+//
+//  rtStream_mpcTests.swift
+//  rtStream-mpcTests
+//
+//  Created by Jens Woltering on 16.01.16.
+//  Copyright © 2016 Jens Woltering. All rights reserved.
+//
+
+import XCTest
+@testable import rtStream_mpc
+
+class rtStream_mpcTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
